ab35ac008eb10983df38aae6f3f5b6a7da77e5d4
Memittal13 <163784314+Memittal13@users.noreply.github.com>
2024-03-29 22:39:08 -0700

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h> 
 using namespace std; 
+// Time Complexity : O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :YEs
+// Any problem you faced while coding this :
   
 // A structure to represent a stack 
 class StackNode { 
 public: 
     int data; 
     StackNode* next; 
+    
 }; 
-  
+
 StackNode* newNode(int data) 
 { 
     StackNode* stackNode = new StackNode(); 
@@ -18,22 +23,45 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+    if(root == NULL) {
+       return 1;  
+    }
+    return 0;
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    //allocate new node and intialize it with data
+    StackNode* node= new StackNode(data);
+ 
+    node->next = *root;
+    *root = node;
 } 
   
 int pop(StackNode** root) 
 { 
-    //Your code here 
+    int top = -1;
+    StackNode *temp;
+    
+    if(!isEmpty(*root)){
+        top = (*root)->data;
+        temp = *root;
+        *root = (*root)->next;
+        delete temp;
+    }
+    
+    return top;
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
+    int top = -1;
+    
+    if(!isEmpty(root)){
+        top = root->data;
+    }
+    
+    return top;
 } 
   
 int main() 
